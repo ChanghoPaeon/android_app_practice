849b08ef5c3a1a0e9f4939868acc6848051b5602
ChanghoPaeon <springer8729@gmail.com>
2018-03-18 19:48:09 +0900

    add onOptionsItemSelected

--- app/src/main/java/com/example/paeon/practice/MainActivity.java
+++ app/src/main/java/com/example/paeon/practice/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.paeon.practice;
 
+import android.content.DialogInterface;
+import android.nfc.Tag;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+   // String TAG = getLocalClassName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +20,26 @@ public class MainActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setTitle(R.string.actionbar_name);
 
+
+
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.action_menu, menu) ;
         return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_settings :
+                Log.d("onOptionsItemSelected", "setting button clicked");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
