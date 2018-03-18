b0302f1f01e154031ff8deb425a0f891ae6f3a51
ChanghoPaeon <springer8729@gmail.com>
2018-03-18 19:14:51 +0900

    add overflow menu on action bar

--- app/src/main/java/com/example/paeon/practice/MainActivity.java
+++ app/src/main/java/com/example/paeon/practice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.paeon.practice;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setTitle(R.string.actionbar_name);
 
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.action_menu, menu) ;
+        return true ;
     }
 }
